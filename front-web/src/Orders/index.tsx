55985f9023d8ab0cd461f246a79f2766eeb9cf5d
Victor <victorcarnevale@yahoo.com.br>
2021-01-06 17:04:08 -0300

    finalizando card & trazaer dados do back

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,27 @@
-import StepsHeader from './StepsHeader';
+import { useEffect, useState } from 'react';
+import { Product } from './types';
 import './styles.css';
+import ProductList from './ProductsList';
+import StepsHeader from './StepsHeader';
+import { fetchProducts } from '../api';
+
+
 
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+    
+    useEffect(() => {
+        fetchProducts()
+        .then(res => setProducts(res.data))
+        .catch(error => console.log(error))
+    },[])
+
     return(
         <div className="orders-container">
            <StepsHeader/>
+           <ProductList products={products}/>
         </div>
     )
 }
