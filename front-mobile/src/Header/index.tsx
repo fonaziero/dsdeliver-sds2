d94301a72e0a02483f05d222eb402b2d2c56a498
Victor <victorcarnevale@yahoo.com.br>
2021-01-10 04:31:24 -0300

    Mobile - add home

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 export default function Header() {
   return (
     <View style={styles.container}>
-        <Image source={require('../assets/logo.png')}></Image>
-      <Text>DS Delivery</Text>
+        <Image style={styles.imgStyle} resizeMode="contain" source={require('../assets/logo.png')}/>
+      <Text style={styles.text}>DS Delivery</Text>
     </View>
   );
 }
@@ -17,5 +17,23 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     flexDirection: 'row',
     justifyContent: 'center'
+
   },
+  text: {
+    display: 'flex',
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: -0.24,
+    color: '#fff',
+    marginRight: 'auto',
+    marginLeft: 15,
+    fontFamily: 'OpenSans_700Bold'
+  },
+  imgStyle: {
+    display: 'flex',
+    height: 'auto',
+    width: 50,
+    marginLeft: 'auto'
+  }
 });
